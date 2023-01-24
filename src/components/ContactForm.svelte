a18d380e223b384298202714b555d08b4d8577ea
Uzziel Kibet <uzzielkk@protonmail.com>
2023-01-24 17:15:27 +0300

    Added contact form

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -2,6 +2,7 @@
 	import axios from "axios"
 	let name
 	let email
+	let phone
 	let message
 	let loading
 	let error
@@ -9,9 +10,10 @@
 
 	const sendMessage = () => {
 		loading = true
-		axios.post(`${import.meta.env.PUBLIC_BACKEND}email`,{
+		axios.post(`/api/contact`,{
 			name,
 			email,
+			phone,
 			message
 		}).then(() => {
 			loading = false
@@ -23,33 +25,64 @@
 		})
 	}
 </script>
-<form class="flex flex-col py-6 space-y-6 md:py-0 md:px-6" on:submit|preventDefault={() => sendMessage()}>
-	<label class="block">
-		<span class="mb-1">Full name</span>
-		<input type="text" placeholder="Leroy Jenkins" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-indigo-400" bind:value={name}>
-	</label>
-	<label class="block">
-		<span class="mb-1">Email address</span>
-		<input type="email" placeholder="leroy@jenkins.com" class="block w-full rounded-md shadow-sm focus:ring focus:ring-opacity-75 focus:ring-indigo-400" bind:value={email}>
-	</label>
-	<label class="block">
-		<span class="mb-1">Message</span>
-		<textarea rows="3" class="block w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-indigo-400" bind:value={message}></textarea>
-	</label>
-	{#if !report}
-		{#if loading}
-			<button type="submit" class="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 focus:ring-indigo-400 hover:ring-indigo-400 bg-indigo-600 text-white">Sending message <span class="animate-ping">...</span></button>
-		{:else}
-			<button type="submit" class="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ring-opacity-75 focus:ring-indigo-400 hover:ring-indigo-400 bg-indigo-600 text-white">Submit</button>
-		{/if}
-	{/if}
-	{#if error}
-		<span class="text-red-600">
+
+<form on:submit|preventDefault={() => sendMessage()}>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="form__grup wow fadeInLeft" data-wow-delay="0.5s">
+          <span class="wpcf7-form-control-wrap" data-name="text-667">
+            <input type="text" bind:value={name} size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Full Name" required />
+          </span>
+          <br />
+          <label for="name" class="form__label">Name (Required)</label>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form__grup wow fadeInLeft" data-wow-delay="0.6s">
+          <span class="wpcf7-form-control-wrap" data-name="email-217">
+            <input type="email" bind:value={email} size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-popup__input" aria-required="true" required aria-invalid="false" placeholder="Email Address" />
+          </span>
+          <br />
+          <label for="email" class="form__label">Email Address (Required)</label>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="form__grup wow fadeInRight" data-wow-delay="0.7s">
+          <span class="wpcf7-form-control-wrap" data-name="text-661">
+            <input type="text" bind:value={phone} size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Phone Number" required />
+          </span>
+          <br />
+          <label for="telefon" class="form__label">Phone</label>
+        </div>
+      </div>
+      <div class="col-sm-12">
+        <div class="form__grup wow fadeInUp" data-wow-delay="0.8s">
+          <span class="wpcf7-form-control-wrap" data-name="textarea-8">
+            <textarea cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea form__input" aria-invalid="false" required placeholder="Your Message" bind:value={message}></textarea>
+          </span>
+          <br />
+          <label for="message" class="form__label">Your Message (Required)</label>
+        </div>
+      </div>
+      <div class="col-sm-12">
+        <div class="form__grup wow fadeInUp" data-wow-delay="0.9s">
+        	{#if loading}
+        		<input type="submit" value="Please wait ..." class="wpcf7-form-control has-spinner wpcf7-submit custom-button" />
+        	{:else}
+	          <input type="submit" value="Contact Us →" class="wpcf7-form-control has-spinner wpcf7-submit custom-button" />
+        	{/if}
+        </div>
+      </div>
+    </div>
+  </div>
+  {#if error}
+		<span class="text-danger">
 			{error}
 		</span>
 	{/if}
 	{#if report}
-		<span class="text-green-600">
+		<span class="text-success">
 			{report}
 		</span>
 	{/if}
---
+++ src/directives/mailer.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+
+export async function sendmail(message,subject,from,name) {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: import.meta.env.MAIL_HOST,
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: import.meta.env.MAIL_USER, // Email account
+      pass: import.meta.env.MAIL_PASS, // password
+    }
+  })
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: `"${name}" <${from}>`, // sender address
+    to: import.meta.env.MAIL_TO, // list of receivers
+    subject: subject, // Subject line
+    html: message, // html body
+  })
+}
